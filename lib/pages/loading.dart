bd8697c620b084f8b05fdaa4f2ab03d569d56aa7
kharioki <antolisah@gmail.com>
2020-05-16 00:21:11 +0300

    worldtime custom class

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
-import 'dart:convert';
+import 'package:worldtime/services/world_time.dart';
 
 class Loading extends StatefulWidget {
   @override
@@ -8,34 +7,32 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
-  void getTime() async {
-    Response response =
-        await get('http://worldtimeapi.org/api/timezone/Africa/Nairobi');
-    Map data = jsonDecode(response.body);
-    // print(data);
+  String time = 'loading';
 
-    // get properties from data
-    String datetime = data['datetime'];
-    String offset = data['utc_offset'].substring(1, 3);
-    // print(datetime);
-    // print(offset);
+  void setupWorldTime() async {
+    WorldTime instance = WorldTime(
+        location: 'Nairobi', url: 'Africa/Nairobi', flag: 'nairobi.png');
 
-    //create DateTime object
-    DateTime now = DateTime.parse(datetime);
-    now = now.add(Duration(hours: int.parse(offset)));
-    print(now);
+    await instance.getTime();
+    print(instance.time);
+    setState(() {
+      time = instance.time;
+    });
   }
 
   @override
   void initState() {
     super.initState();
-    getTime();
+    setupWorldTime();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('Loading screen'),
+      body: Padding(
+        padding: const EdgeInsets.all(50.0),
+        child: Text(time),
+      ),
     );
   }
 }
